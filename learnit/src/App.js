0508b42365b546e97ac77b9dad6da752162fa65a
Alexander Villamil J <didier.villamil@cun.edu.co>
2024-09-04 00:10:53 -0500

    acoplamiento de react a learnit

---
+++ learnit/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Bienvenido a learnit, plataforma de aprendizaje de nuevas tecnologías
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
